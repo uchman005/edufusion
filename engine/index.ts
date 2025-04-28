049b2371a094833721ae147caeea813e519851d2
Obiefuna Marcel Uchenna <obiefunamarcel@gmail.com>
2025-04-28 12:22:12 +0100

    Using Bun

---
+++ engine/index.ts
@@ -0,0 +1,37 @@
+import html from "@elysiajs/html";
+import staticPlugin from "@elysiajs/static";
+import Elysia, { t } from "elysia";
+
+new Elysia()
+  .use(
+    staticPlugin({
+      prefix: "/public",
+      assets: "./public",
+    })
+  )
+  .use(html())
+  .get("/", () => Bun.file("./public/index.html"))
+  .post("/submit", ({body})=>{
+    const {name, email, school, phone, message} = body;
+    const emailBody = `
+    Request Details:
+    -------------------
+    Name: ${name}
+    School: ${school}
+    Email: ${email}
+    Phone: ${phone}
+    Additional Information: ${message || 'None provided'}
+  `.trim();
+  
+  }, {
+    body: t.Object({
+      name: t.String(),
+      email: t.String({format: "email"}),
+      school: t.String(),
+      phone: t.String(),
+      message: t.String()
+    })
+  })
+  .listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
+  });
