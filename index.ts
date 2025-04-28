bf429c3503f8c5a066d7f3f1b64cca5908b19f42
Obiefuna Marcel Uchenna <obiefunamarcel@gmail.com>
2025-04-28 13:39:39 +0100

    Added emailing functionality

--- index.ts
+++ index.ts
@@ -17,15 +17,39 @@ new Elysia()
     async ({ body }) => {
       const { name, email, school, phone, message } = body;
       const emailBody = `
-    Request Details:
-    -------------------
-    Name: ${name}
-    School: ${school}
-    Email: ${email}
-    Phone: ${phone}
-    Additional Information: ${message || "None provided"}
-  `.trim();
-      await sendMail("godwinudu01@gmail.com", name, "New Request", emailBody);
+        <!DOCTYPE html>
+      <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to KenEi Health</title>
+        <style>
+          body { font-family: Arial, sans-serif; background-color: #f4f4f4; }
+          .container { max-width: 600px; margin: 20px auto; background: #fff; padding: 20px; border-radius: 8px; }
+          .header { text-align: center; font-size: 24px; color: #333; }
+          .content { font-size: 16px; color: #555; text-align: center; }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <h1 class="header">Request Details</h1>
+          -------------------
+          <div class="content">
+            <p>Name: ${name}</p>
+            <p>School: ${school}</p>
+            <p>Email: ${email}</p>
+            <p>Phone: ${phone}</p>
+            <p>Additional Information: ${message || "None provided"}</p>
+          </div>
+        </div>
+      </body> 
+  `;
+      await sendMail(
+        "godwinudu01@gmail.com",
+        name,
+        "New Request",
+        emailBody
+      );
       return { success: true, message: "Request submitted successfully" };
     },
     {
